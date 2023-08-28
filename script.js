d37b655fee60e71be5ec070839351b1e50586e5d
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-08-28 23:41:27 +0530

    Js 3

--- script.js
+++ script.js
@@ -21,3 +21,42 @@ handleSlider();
 // Set strength circle color to grey
 setIndicator("#ccc")
 
+// Set Password Length
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+
+    inputSlider.style.backgroundSize = (
+        (passwordLength - min) * 100 / (max - min)
+    ) + "% 100"
+}
+
+function setIndicator(color) {
+    indicator.style.backgroundColor = color;
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomNumber() {
+    return getRndInteger(0, 9);
+}
+
+function generateLowerCase() {
+    return String.fromCharCode(getRndInteger(97, 123))
+}
+
+function generateUpperCase() {
+    return String.fromCharCode(getRndInteger(65, 91))
+}
+
+function generateSymbol() {
+    const randNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
+
